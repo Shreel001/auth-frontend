bfe49acef2cf10e145b116b0ae3b290425dd8a09
Shreel100 <patelshreel001@gmail.com>
2026-01-19 12:43:37 -0500

    Error logging 10

--- src/pages/Authentication/signUp.jsx
+++ src/pages/Authentication/signUp.jsx
@@ -81,20 +81,24 @@ export default function UserSignUpForm() {
             </div>
 
             <div>
-              <label htmlFor="role">Role:</label>
-              <select
-                id="role"
-                name="role"
-                required
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-              >
-                <option value="">Select a role</option>
-                <option value="ACCOUNT MANAGER">ACCOUNT MANAGER</option>
-                <option value="ADMIN">ADMIN</option>
-                <option value="API USER">API USER</option>
-              </select>
-            </div>
+                <label for="role">Role:</label>
+                <select id="role" name="role" onChange={(e) => setRole(e.target.value)} value={role} required>
+                    <option value="">Select a role</option>
+                    <option value="ACCOUNT MANAGER">ACCOUNT MANAGER</option>
+                    <option value="ADMIN">ADMIN</option>
+                    <option value="API USER">API USER</option>
+                    <option value="AUDIT SERVICE">AUDIT SERVICE</option>
+                    <option value="BRANCH MANAGER">BRANCH MANAGER</option>
+                    <option value="COMPLIANCE OFFICER">COMPLIANCE OFFICER</option>
+                    <option value="CORPORATE CUSTOMER">CORPORATE CUSTOMER</option>
+                    <option value="CUSTOMER">CUSTOMER</option>
+                    <option value="CUSTOMER REP">CUSTOMER REP</option>
+                    <option value="LOAN OFFICER">LOAN OFFICER</option>
+                    <option value="PERSONAL BANKER">PERSONAL BANKER</option>
+                    <option value="PREMIUM CUSTOMER">PREMIUM CUSTOMER</option>
+                    <option value="REGIONAL MANAGER">REGIONAL MANAGER</option>
+                    <option value="REPORTING SERVICE">REPORTING SERVICE</option>
+                    <option value="SECURITY ADMIN">SECURITY ADMIN</option> </select> </div>
             
             <button type="submit">Sign Up</button>
 
